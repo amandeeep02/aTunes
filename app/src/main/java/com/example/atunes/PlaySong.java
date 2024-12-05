617d6534ffd9a2b68c8aaf4edcdf39f06cc102a4
amandeeep02 <amandeepsr02@gmail.com>
2024-12-06 01:53:49 +0530

    fix: Next Song automatic playing

--- app/src/main/java/com/example/atunes/PlaySong.java
+++ app/src/main/java/com/example/atunes/PlaySong.java
@@ -1,31 +1,18 @@
 package com.example.atunes;
 
-
-import static android.telephony.TelephonyCallback.*;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.telephony.PhoneStateListener;
-import android.telephony.ServiceState;
-import android.telephony.TelephonyCallback;
-import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.webkit.WebView;
-
 
 import java.io.File;
 import java.util.ArrayList;
@@ -33,31 +20,35 @@ import java.util.concurrent.TimeUnit;
 
 public class PlaySong extends AppCompatActivity {
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mediaPlayer.stop();
-        mediaPlayer.release();
-        updateSeek.interrupt();
-
-    }
-
     TextView textView, firstDuration, secondDuration;
-    ImageView play, previous, next, pause;
+    ImageView play, previous, next;
     ArrayList<File> songs;
     MediaPlayer mediaPlayer;
     String textContent;
     int position;
     SeekBar seekBar;
     Thread updateSeek;
-    boolean stopThread;
+    boolean stopThread = false;
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+        stopThread = true; // Stop the thread when activity is destroyed
+        if (updateSeek != null) {
+            updateSeek.interrupt();
+        }
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
+
         textView = findViewById(R.id.textView);
         play = findViewById(R.id.play);
         previous = findViewById(R.id.previous);
@@ -66,148 +57,128 @@ public class PlaySong extends AppCompatActivity {
         firstDuration = findViewById(R.id.firstDuration);
         secondDuration = findViewById(R.id.secondDuration);
 
-
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         songs = (ArrayList) bundle.getParcelableArrayList("songList");
         textContent = intent.getStringExtra("currentSong");
+        position = intent.getIntExtra("position", 0);
+
         textView.setText(textContent);
         textView.setSelected(true);
-        position = intent.getIntExtra("position", 0);
-        Uri uri = Uri.parse(songs.get(position).toString());
-        mediaPlayer = MediaPlayer.create(this, uri);
-        mediaPlayer.start();
-        seekBar.setMax(mediaPlayer.getDuration());
 
+        playSong();
+
+        // SeekBar listener
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                if (fromUser && mediaPlayer != null) {
+                    mediaPlayer.seekTo(progress);
+                }
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                mediaPlayer.seekTo(seekBar.getProgress());
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        // Play/pause button
+        play.setOnClickListener(view -> {
+            if (mediaPlayer.isPlaying()) {
+                play.setImageResource(R.drawable.play);
+                mediaPlayer.pause();
+            } else {
+                play.setImageResource(R.drawable.pause);
+                mediaPlayer.start();
             }
         });
 
-        this.updateSeek = new Thread() {
-            public void run() {
-                while (!PlaySong.this.stopThread) {
-                    try {
-                        if (PlaySong.this.mediaPlayer != null) {
-                            long currentPosition = (long) PlaySong.this.mediaPlayer.getCurrentPosition();
-                            final String format = String.format("%02d:%02d", new Object[]{Long.valueOf(TimeUnit.MILLISECONDS.toMinutes(currentPosition)), Long.valueOf(TimeUnit.MILLISECONDS.toSeconds(currentPosition) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(currentPosition)))});
-                            PlaySong.this.runOnUiThread(new Runnable() {
-                                public void run() {
-                                    PlaySong.this.firstDuration.setText(format);
-                                }
-                            });
-                            PlaySong.this.seekBar.setProgress(PlaySong.this.mediaPlayer.getCurrentPosition());
-                            sleep(200);
-                            Log.d("threadCode", "Updating Success");
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Log.d("threadCode", "Updating Failed");
-                    }
-                }
+        // Previous button
+        previous.setOnClickListener(view -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
             }
-        };
+            position = (position - 1 + songs.size()) % songs.size();
+            playSong();
+        });
 
-        this.play.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (PlaySong.this.mediaPlayer.isPlaying()) {
-                    PlaySong.this.play.setImageResource(R.drawable.play);
-                    PlaySong.this.mediaPlayer.pause();
-                    return;
-                }
-                PlaySong.this.play.setImageResource(R.drawable.pause);
-                PlaySong.this.mediaPlayer.start();
+        // Next button
+        next.setOnClickListener(view -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
             }
+            position = (position + 1) % songs.size();
+            playSong();
         });
-        this.previous.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                PlaySong.this.mediaPlayer.stop();
-                PlaySong.this.mediaPlayer.release();
-                if (PlaySong.this.position != 0) {
-                    PlaySong.this.position--;
-                } else {
-                    PlaySong playSong = PlaySong.this;
-                    playSong.position = playSong.songs.size() - 1;
-                }
-                Uri parse = Uri.parse(PlaySong.this.songs.get(PlaySong.this.position).toString());
-                PlaySong playSong2 = PlaySong.this;
-                playSong2.mediaPlayer = MediaPlayer.create(playSong2.getApplicationContext(), parse);
-                PlaySong.this.mediaPlayer.start();
-                PlaySong.this.play.setImageResource(R.drawable.pause);
-                PlaySong.this.seekBar.setMax(PlaySong.this.mediaPlayer.getDuration());
-                PlaySong playSong3 = PlaySong.this;
-                playSong3.textContent = playSong3.songs.get(PlaySong.this.position).getName().toString();
-                PlaySong.this.textView.setText(PlaySong.this.textContent);
-                long duration = (long) PlaySong.this.mediaPlayer.getDuration();
-                PlaySong.this.secondDuration.setText(String.format("%02d:%02d", new Object[]{Long.valueOf(TimeUnit.MILLISECONDS.toMinutes(duration)), Long.valueOf(TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)))}));
-            }
-        });
-        this.next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                PlaySong.this.mediaPlayer.stop();
-                PlaySong.this.mediaPlayer.release();
-                if (PlaySong.this.position != PlaySong.this.songs.size() - 1) {
-                    PlaySong.this.position++;
-                } else {
-                    PlaySong.this.position = 0;
-                }
-                Uri parse = Uri.parse(PlaySong.this.songs.get(PlaySong.this.position).toString());
-                PlaySong playSong = PlaySong.this;
-                playSong.mediaPlayer = MediaPlayer.create(playSong.getApplicationContext(), parse);
-                PlaySong.this.mediaPlayer.start();
-                PlaySong.this.play.setImageResource(R.drawable.pause);
-                PlaySong.this.seekBar.setMax(PlaySong.this.mediaPlayer.getDuration());
-                PlaySong playSong2 = PlaySong.this;
-                playSong2.textContent = playSong2.songs.get(PlaySong.this.position).getName().toString();
-                PlaySong.this.textView.setText(PlaySong.this.textContent);
-                long duration = (long) PlaySong.this.mediaPlayer.getDuration();
-                PlaySong.this.secondDuration.setText(String.format("%02d:%02d", new Object[]{Long.valueOf(TimeUnit.MILLISECONDS.toMinutes(duration)), Long.valueOf(TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)))}));
-            }
-        });
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                PlaySong.this.mediaPlayer.stop();
-                PlaySong.this.mediaPlayer.release();
-                if (PlaySong.this.position != PlaySong.this.songs.size() - 1) {
-                    PlaySong.this.position++;
-                } else {
-                    PlaySong.this.position = 0;
-                }
-                Uri parse = Uri.parse(PlaySong.this.songs.get(PlaySong.this.position).toString());
-                PlaySong playSong = PlaySong.this;
-                playSong.mediaPlayer = MediaPlayer.create(playSong.getApplicationContext(), parse);
-                PlaySong.this.mediaPlayer.start();
-                PlaySong.this.play.setImageResource(R.drawable.pause);
-                PlaySong.this.seekBar.setMax(PlaySong.this.mediaPlayer.getDuration());
-                PlaySong playSong2 = PlaySong.this;
-                playSong2.textContent = playSong2.songs.get(PlaySong.this.position).getName().toString();
-                PlaySong.this.textView.setText(PlaySong.this.textContent);
-                long duration = (long) PlaySong.this.mediaPlayer.getDuration();
-                PlaySong.this.secondDuration.setText(String.format("%02d:%02d", new Object[]{Long.valueOf(TimeUnit.MILLISECONDS.toMinutes(duration)), Long.valueOf(TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)))}));
-            }
-        });
-        long duration = (long) this.mediaPlayer.getDuration();
-        this.secondDuration.setText(String.format("%02d:%02d", new Object[]{Long.valueOf(TimeUnit.MILLISECONDS.toMinutes(duration)), Long.valueOf(TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)))}));
-        this.updateSeek.start();
-
-
     }
 
+    private void playSong() {
+        Uri uri = Uri.parse(songs.get(position).toString());
+        mediaPlayer = MediaPlayer.create(this, uri);
+        mediaPlayer.start();
 
+        // Update UI
+        play.setImageResource(R.drawable.pause);
+        textContent = songs.get(position).getName();
+        textView.setText(textContent);
 
+        // Set seek bar max and duration
+        seekBar.setMax(mediaPlayer.getDuration());
+        long duration = mediaPlayer.getDuration();
+        secondDuration.setText(String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
+        ));
 
+        // Handle seek bar updates
+        startSeekBarUpdateThread();
 
+        // On completion, move to the next song
+        mediaPlayer.setOnCompletionListener(mp -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+            position = (position + 1) % songs.size();
+            playSong();
+        });
+    }
+
+    private void startSeekBarUpdateThread() {
+        updateSeek = new Thread(() -> {
+            while (!stopThread) {
+                try {
+                    if (mediaPlayer != null) {
+                        synchronized (mediaPlayer) {
+                            if (mediaPlayer.isPlaying()) {
+                                int currentPosition = mediaPlayer.getCurrentPosition();
+                                seekBar.setProgress(currentPosition);
+                                runOnUiThread(() -> {
+                                    firstDuration.setText(formatDuration(currentPosition));
+                                });
+                            }
+                        }
+                    }
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (IllegalStateException e) {
+                    Log.e("PlaySong", "MediaPlayer is in an illegal state", e);
+                } catch (InterruptedException e) {
+                    Log.e("PlaySong", "SeekBar update thread interrupted", e);
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        updateSeek.start();
+    }
+
+    private String formatDuration(int duration) {
+        int minutes = (duration / 1000) / 60;
+        int seconds = (duration / 1000) % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
 }
